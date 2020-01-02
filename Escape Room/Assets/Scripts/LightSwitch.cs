32a927799a1b4f63b73ffcda5ca986ee282b76f3
Nick1441 <43572914+Nick1441@users.noreply.github.com>
2020-01-02 21:23:42 +0000

    Created Lights
    - Created Lights Animation/On/Off
    - Created Swapping Texture for Light Switch On/Off.
    - Created Fading In Animation for lights.
    - Created Dark Scene So Room Will Go Dark.
    
    To Do
    - Fix Room So it is also Lit Up byu Lights.

---
+++ Escape Room/Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    public GameObject SwitchOn;
+    public GameObject SwitchOff;
+    public GameObject[] Lights;
+    public float Duration = 3f;
+
+    public Material Mat;
+    public Color ColorOn = new Vector4(67, 67, 62, 1);
+    public Color ColorOff = new Vector4(67, 67, 62, 0);
+    public Transform Camera;
+
+    public float RayDistance = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Lights = GameObject.FindGameObjectsWithTag("Light");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwitchCheck();
+    }
+
+    public void SwitchCheck()
+    {
+        RaycastHit Ray;
+        if (Physics.Raycast(Camera.position, transform.TransformDirection(Camera.forward), out Ray, RayDistance))
+        {
+            Debug.DrawRay(Camera.position, transform.TransformDirection(Camera.forward) * Ray.distance, Color.red);
+            if (Ray.transform.tag == "SwitchOn")
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Debug.Log("Contact");
+                    OffLights();
+                }
+            }
+            else if (Ray.transform.tag == "SwitchOff")
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Debug.Log("Contact");
+                    OnLights();
+                }
+            }
+        }
+    }
+
+    public void OnLights()
+    {
+        SwitchOn.SetActive(true);
+        SwitchOff.SetActive(false);
+
+        for (int i = 0; i < Lights.Length; i++)
+        {
+            Lights[i].GetComponent<Animator>().SetBool("On", false);
+            //Lights[i].GetComponent<Light>().intensity = 2.61f;
+            //Lights[i].GetComponent<Light>().color = Color.Lerp(ColorOff, ColorOn, Duration);
+        }
+    }
+
+    public void OffLights()
+    {
+        SwitchOn.SetActive(false);
+        SwitchOff.SetActive(true);
+
+        for (int i = 0; i < Lights.Length; i++)
+        {
+            Lights[i].GetComponent<Animator>().SetBool("On", true);
+            //Lights[i].GetComponent<Light>().intensity = 0f;
+            //Lights[i].GetComponent<Light>().color = Color.Lerp(ColorOn, ColorOff, Duration);
+        }
+    }
+}
