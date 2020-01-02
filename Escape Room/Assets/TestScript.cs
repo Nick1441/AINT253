fb7667333f912a642cc37bac3d4f77563d3718ed
Nick1441 <43572914+Nick1441@users.noreply.github.com>
2020-01-02 22:40:29 +0000

    Created Torch For Puzzle 1.
    - Created 3D Torch In Blender.
    - Strated Creating Hand For Torch.

---
+++ Escape Room/Assets/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("HIT");
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+}
